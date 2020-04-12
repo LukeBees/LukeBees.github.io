feddb7ef09148fb18d1aa25eb3d4fc883b6618ad
LukeBees <61568563+LukeBees@users.noreply.github.com>
2020-04-12 13:08:06 -0500

    Update connect.js

--- connect.js
+++ connect.js
@@ -2,60 +2,91 @@
 var canvas = document.getElementbyId('canvas');
 var ctx = canvas.getContext('2d');
 
-let allChips = new Array(6);
+var allChips = new Array(7);
 for(let i = 0; i < allChips.length; i++)
-  {
-	  allChips[i] = new Array(7);
-	  for(let j = 0; j < 7; j++)
-	  {
-		  allChips[i][j] = 0;
-	  }
-	  ctx.strokeRect(10,10,50,50);
-  }
+{
+	allChips[i] = new Array(6);
+	for(let j = 0; j < 6; j++)
+	{
+		allChips[i][j] = 0;
+	}
+}
 
 function resetBoard()
 {
-  for(let i = 0; i < allChips.length; i++)
-  {
-	  for(let j = 0; j < 7; j++)
-	  {
-		  allChips[i][j] = 0;
-	  }  
-  }
+	for(let i = 0; i < allChips.length; i++)
+	{
+		for(let j = 0; j < 6; j++)
+		{
+			allChips[i][j] = 0;
+		}
+	}
+}
+
+function drawBoard()
+{
+	for(let i = 0; i < 7; i++)
+	{
+		for(let j = 0; j < 6; j++)
+		{
+			if(allChips[i][j] == 0)
+			{
+				ctx.beginPath();
+				ctx.arc((40+50*i), (40+50*j), 20, 0, 2 * Math.PI);
+				ctx.stroke();
+			}
+			else if(allChips[i][j] == 1)
+			{
+				ctx.beginPath();
+				ctx.fillStyle = 'red';
+				ctx.arc((40+50*i), (40+50*j), 20, 0, 2 * Math.PI);
+				ctx.fill();			}
+			else //allChips[i][j] == 2
+			{
+				ctx.beginPath();
+				ctx.fillStyle = 'yellow';
+				ctx.arc((40+50*i), (40+50*j), 20, 0, 2 * Math.PI);
+				ctx.fill();
+			}
+		}
+	}
 }
 
 function connectfour() 
 {
-  let isWin = false;  
-  let t = 0;
+	let isWin = false;  
+	let t = 0;
   
-  while(!isWin || t < 49)
-  {
-    if(t % 2 == 0) //red plays
-    {
-      let y;
-      if(isValid(y))
-      {
-        add(y, 1);
-        t++;
-      }
-    }
-    else //t % 2 == 1, yellow plays
-    {
-      let y;
-      if(isValid(y))
-      {
-        add(y, 2);
-        t++;
-      }
-    }
+	while(!isWin || t < 49)
+	{
+	
+	drawBoard();
+		
+	if(t % 2 == 0) //red plays
+	{
+		let y;
+		if(isValid(y))
+      		{
+        		add(y, 1);
+        		t++;
+      		}
+    	}
+    	else //t % 2 == 1, yellow plays
+    	{
+      		let y;
+      		if(isValid(y))
+      		{
+        		add(y, 2);
+        		t++;
+      		}
+   	}
 	  
-    if(t > 7) //a minimum of seven turns overall is needed for the first player to add four chips, so only then does the program check for winners
-    {
-      isWin = checkWin();
-    }
-  }
-  
+    	if(t > 7) //a minimum of seven turns overall is needed for the first player to add four chips, so only then does the program check for winners
+    	{
+      	isWin = checkWin();
+    	}
+
+  	}
 }
      
 function add(c, s) //c represents the column (0-6), s represents the side
